57a1380bfa6a382118c093764299845dfc61721d
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-20 14:45:45 +0200

    added array of objects

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Definisco l'array di oggetti
+const byciles = [
+    {
+        nome: 'Van Rysel fcr',
+        peso: 8,
+    }, 
+    {
+        nome: 'Bianchi Oltre Comp',
+        peso: 7.8,
+    },
+    {
+        nome: 'Pinarello Dogma F12',
+        peso: 7.4,
+    },
+    {
+        nome: 'Trek Madone SL',
+        peso: 8.74,
+    }
+];
