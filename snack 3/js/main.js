105456a2bcd104e37b164768198d5f43c45bff6e
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-20 21:21:29 +0200

    added array

---
+++ snack 3/js/main.js
@@ -0,0 +1,17 @@
+const boards = [
+    'Drake',
+    'K2',
+    'Burton',
+    'Solomon',
+    'Bataleon',
+    'Jones',
+    'Capita',
+    'Funky',
+    'Gnu',
+    'Nitro',
+    'Ride',
+    'Rome',
+    'Roxy'
+];
+
+console.log(boards);
