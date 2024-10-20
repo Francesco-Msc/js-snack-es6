4485fc8080210814f48adada7231f65b587b6530
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-20 21:25:06 +0200

    added function that create a new array with only a specific portion of the original array using two numbers as index limits

--- snack 3/js/main.js
+++ snack 3/js/main.js
@@ -15,3 +15,25 @@ const boards = [
 ];
 
 console.log(boards);
+
+const a = 1;
+const b = 7;
+
+/********
+FUNZIONE 
+********/
+/**
+ * funzione che prende un array e due valori e se questi due valori sono numerici mi restituisce un nuovo array con gli elementi compresi tra i due numeri 
+ * @param {array} boards
+ * @param {number} a
+ * @param {number} b
+ * @returns {array}
+ */
+function myBestBoards(boards, a, b){
+    if (!isNaN(a) && !isNaN(b) && a < b){
+        return boards.slice(a, b);
+    };
+}
+
+const result = myBestBoards(boards, a, b)
+console.log(result);
