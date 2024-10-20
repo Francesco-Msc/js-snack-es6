9f50c463de9428f9fc416b41747433ece9c80228
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-20 16:06:10 +0200

    added array of objects

---
+++ snack 2/js/main.js
@@ -0,0 +1,27 @@
+const teams = [
+    {
+        teamName: 'Porto',
+        scores: 0,
+        recivedFouls: 0
+    },
+    {
+        teamName: 'Juventus',
+        scores: 0,
+        recivedFouls: 0
+    },
+    {
+        teamName: 'Liverpool',
+        scores: 0,
+        recivedFouls: 0
+    },
+    {
+        teamName: 'Bayern Monaco',
+        scores: 0,
+        recivedFouls: 0
+    },
+    {
+        teamName: 'Real Madrid',
+        scores: 0,
+        recivedFouls: 0
+    },
+];
